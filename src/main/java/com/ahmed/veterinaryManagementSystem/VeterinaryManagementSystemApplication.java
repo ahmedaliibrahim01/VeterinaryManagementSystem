5b4410d82cf9f3821789bfbf0cf2149b48dfcde8
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-05-13 06:04:00 +0300

    configrations

---
+++ src/main/java/com/ahmed/veterinaryManagementSystem/VeterinaryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ahmed.veterinaryManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmed/veterinaryManagementSystem/VeterinaryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmed.veterinaryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
